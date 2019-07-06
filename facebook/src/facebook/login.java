a5965f3a55e4edbfa39f66749c1a84c498d5a241
prajaktasud <prajaktasud98@gmail.com>
2019-07-06 12:53:04 +0530

    second commit by dev2-p

---
+++ facebook/src/facebook/login.java
@@ -0,0 +1,12 @@
+package facebook;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Facebook by dev1");
+		System.out.println("How are you?");
+
+	}
+
+}
